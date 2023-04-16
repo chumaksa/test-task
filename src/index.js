876b7aa8b6cb7cfa89335a02439e9a18f6ae740b
Sergey Chumak <for_serega@mail.ru>
2023-04-16 22:20:29 +0300

    feat: implement carousel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 import './styles/main.scss';
+
+const btnNext = document.querySelector('#next');
+const btnPrev = document.querySelector('#prev');
+const carouselLine = document.querySelector('.carousel__line');
+
+
+const lineWidth = carouselLine.scrollWidth;
+const slideGap = parseInt(getComputedStyle(carouselLine).gap);
+const countOfSlides = carouselLine.children.length;
+const slideWidth = (lineWidth - slideGap * (countOfSlides - 1)) / countOfSlides;
+
+const getOffset = () => parseInt(getComputedStyle(carouselLine).left);
+
+const changeStateButton = () => {
+  const currentOffset = getOffset();
+  btnPrev.disabled = currentOffset === 0;
+  btnNext.disabled = (lineWidth + currentOffset) <= (3 * slideWidth + 2 * slideGap);
+}
+changeStateButton();
+
+const moveRight = () => {
+  carouselLine.style.left = getOffset() + slideWidth + slideGap + 'px';
+  btnPrev.removeEventListener('click', moveRight);
+  btnNext.removeEventListener('click', moveLeft);
+}
+
+const moveLeft = () => {
+  carouselLine.style.left = getOffset() - slideWidth - slideGap + 'px';
+  btnPrev.removeEventListener('click', moveRight);
+  btnNext.removeEventListener('click', moveLeft);
+}
+
+btnPrev.addEventListener('click', moveRight)
+
+btnNext.addEventListener('click', moveLeft)
+
+carouselLine.addEventListener('transitionend', () => {
+  changeStateButton();
+  btnPrev.addEventListener('click', moveRight);
+  btnNext.addEventListener('click', moveLeft);
+});
