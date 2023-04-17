20d1eb18974dc0f98e164089de86af19dcfa1503
Sergey Chumak <for_serega@mail.ru>
2023-04-17 15:56:49 +0300

    feat: implement like functionality

--- src/index.js
+++ src/index.js
@@ -8,3 +8,12 @@ carouselLine.addEventListener('transitionend', () => {
   changeStateButton();
   addBtnListener();
 });
+
+const btnRoundLikeList = document.querySelectorAll('.card__button');
+btnRoundLikeList.forEach((btn) => {
+  btn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    evt.currentTarget.classList.toggle('card__button_active');
+    evt.currentTarget.querySelector('.btn-round-like').classList.toggle('btn-round-like_active');
+  })
+})
